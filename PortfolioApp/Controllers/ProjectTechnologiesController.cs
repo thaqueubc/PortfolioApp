76dba69ab7e11e1d6ea597fc99d10a176b1d3cb7
A01174842 <A01174842@DTC01L0382-24>
2019-12-06 14:30:25 -0800

    Attribute Routing Added

---
+++ PortfolioApp/Controllers/ProjectTechnologiesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PortfolioApp.Models;
+
+namespace PortfolioApp.Controllers
+{
+    [Route("TasnuvaHaque")]
+    public class ProjectTechnologiesController : Controller
+    {
+        private PortfolioContext db;
+        public ProjectTechnologiesController(PortfolioContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(db.TechnologyProjects.Include(tp=>tp.Project).Include(tp=>tp.Technology));
+        }
+    }
+}
