e8dc0c9761bc115118292716b8e3761285d85189
A01174842 <A01174842@DTC01L0382-24>
2019-12-06 12:59:33 -0800

    Add project files.

---
+++ PortfolioApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PortfolioApp.Models;
+
+namespace PortfolioApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private PortfolioContext db;
+
+        // Initialize context when controller is created.
+        public HomeController(PortfolioContext db)
+        {
+            this.db = db;
+            Seeder seeder = new Seeder(db);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
